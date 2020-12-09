f4e2dbede1b35752b0443172bc60dd775f43eb5f
Walter Hilton <walter.hilton@amobee.com>
2020-12-08 21:20:24 -0500

    Sort and prettify output a little

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,14 @@
 import scala.io.Source
 
 object Main extends App {
+
   val artists: List[List[String]] = Source.fromFile("./Artist_lists_small.txt").getLines.toList.map(_.split(",").toList)
 
-  naive.repeatedPairs(artists).foreach(println)
+  val result00 = naive.repeatedPairs(artists)
+  prepareOutput(result00.toList).foreach(println)
+
+  def prepareOutput(pairs: List[(String, String)]): List[String] =
+    pairs.sorted.map {
+      case(l, r) => s"$l,$r"
+    }
 }
